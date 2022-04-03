fa8f09fb419000ae9f3be54743c2710d9355fd71
Stiven Arango <49137539+wstivenarango@users.noreply.github.com>
2022-04-03 18:56:23 -0500

    Add files via upload

---
+++ Carrito-Compras-JavaScript-0.0.0/js/compra.js
@@ -0,0 +1,27 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector("#lista-compra tbody");
+const carrito = document.getElementById('carrito');
+const procesarCompraBtn = document.getElementById('procesar-compra');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
+
+
+cargarEventos();
+
+function cargarEventos() {
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+
+    //Eliminar productos del carrito
+    carrito.addEventListener('click', (e) => { compra.eliminarProducto(e) });
+
+    compra.calcularTotal();
+
+    //cuando se selecciona procesar Compra
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+    carrito.addEventListener('change', (e) => { compra.obtenerEvento(e) });
+    carrito.addEventListener('keyup', (e) => { compra.obtenerEvento(e) });
+
+
+}
+
